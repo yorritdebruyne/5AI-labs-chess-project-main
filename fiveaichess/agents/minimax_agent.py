bfff3b59dd7543829273769c3a7dd3fe9efa8253
Yorrit <yorritdebruyne@gmail.com>
2025-12-06 14:31:51 +0100

    Fully implemented alpha-beta pruning

--- fiveaichess/agents/minimax_agent.py
+++ fiveaichess/agents/minimax_agent.py
@@ -10,9 +10,13 @@ from fiveaichess.agents.agent import Agent
 from fiveaichess.utilities.utility import Utility
 
 """
-An example of a chess agent that provides a naive implementation
-
-This agent iterates trough all the moves possible and picks the one with the highest utility.
+Minimax Agent with alpha-beta pruning
+-------------------------------------
+This agent uses a search algorithm to evaluate moves.
+He can use minimax with or without alpha-beta pruning.
+Alpha-beta pruning realises the same output as the classic minimax,
+but calculates faster by pruning the branches 
+when they are not relevant anymore.
 
 """
 
@@ -24,8 +28,12 @@ class MinimaxAgent(Agent):
 
     def __init__(self, utility : Utility, time_limit_move : float, depth):
         super().__init__(utility, time_limit_move)
-        self.depth = depth # Save in class
+        # Save in class
+        self.depth = depth
 
+    # ---------------------------------------
+    # --- Classic minimax without pruning ---
+    # ---------------------------------------
 
     # Recursively simulates future moves to evaluate board positions
     def minimax(self, board : Board , depth : int, maximizing_player : bool):
@@ -61,6 +69,10 @@ class MinimaxAgent(Agent):
                 # Evaluate values until the optimal move is determined
                 worst_value = min(value, worst_value)
             return worst_value
+
+    #-----------------------------------
+    # --- Alpha-beta pruning version ---
+    # ----------------------------------
 
     def alphabeta(self, board : Board, depth : int, alpha : float, beta : float, maximizing_player : bool):
         # If depth = empty or the game is over: give evaluation back
@@ -103,9 +115,13 @@ class MinimaxAgent(Agent):
                     break  # Prune: no value in searching any further
             return worst_value
 
-    # Chooses the best current move by evaluating all options with minimax
+    # ----------------------
+    # --- Move Selection ---
+    # ----------------------
+
+    # Chooses the best current move by evaluating all options with alpha-beta pruning
     def calculate_move(self, board: Board, constraints: dict[str, int] = {}) -> Move:
-        startTime = time.time()
+        start_time = time.time()
 
         best_move = None
 
@@ -113,13 +129,26 @@ class MinimaxAgent(Agent):
         # BLACK = minimzing player: wants to minimize the player's score
         maximizing_player = True if board.turn == chess.WHITE else False
 
+        alpha = - float('inf')
+        beta = float('inf')
+
         if maximizing_player:
             best_value = - float('inf')
             for move in list(board.legal_moves):
+
+                # Check if the maximum calculation time for this move has been reached
+                if time.time() - start_time > self.time_limit_move:
+                    break # When time is exceeded
+
                 # Play move
                 board.push(move)
-                # Calculate new value
-                value = self.minimax(board, self.depth - 1, False)
+
+                # Calculate new value with minimax
+#                value = self.minimax(board, self.depth - 1, False)
+
+                # Calculate new value with alphabeta pruning
+                value = self.alphabeta(board, self.depth - 1, alpha, beta, False)
+
                 # Reverse move
                 board.pop()
 
@@ -134,10 +163,20 @@ class MinimaxAgent(Agent):
         else:
             worst_value = float('inf')
             for move in list(board.legal_moves):
+
+                # Check if the maximum calculation time for this move has been reached
+                if time.time() - start_time > self.time_limit_move:
+                    break # When time is exceeded
+
                 # Play move
                 board.push(move)
-                # Calculate new value
-                value = self.minimax(board, self.depth - 1, True)
+
+                # Calculate new value with minimax
+#                value = self.minimax(board, self.depth - 1, True)
+
+                # Calculate new value with alphabeta pruning
+                value = self.alphabeta(board, self.depth - 1, alpha, beta, True)
+
                 # Reverse move
                 board.pop()
 
